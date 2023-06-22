fa383e9afdac6fc16032c5cac5a28c622c2ff3a7
Steven <steven@steventan.com.au>
2023-06-22 16:04:09 +1000

    feat: increase maxY

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import getChartOptions from "./modules/chart-options";
-import drawChart from "./modules/chart";
-import dataTransform from "./modules/data-transform";
-import rollUp from "./modules/roll-up";
-import { get7dPowerPath } from "./modules/json-paths"
-import * as nemFts from "./modules/fuel-techs-nem";
-import * as wemFts from "./modules/fuel-techs-wem";
+import getChartOptions from './modules/chart-options';
+import drawChart from './modules/chart';
+import dataTransform from './modules/data-transform';
+import rollUp from './modules/roll-up';
+import { get7dPowerPath } from './modules/json-paths';
+import * as nemFts from './modules/fuel-techs-nem';
+import * as wemFts from './modules/fuel-techs-wem';
 
-const region = "NEM"; // or "WEM"
-const fts = region === "NEM" ? nemFts : wemFts;
-const regionLength = region === "NEM" ? 2017 : 337;
-const is5Min = region === "NEM" ? true : false;
-const useGW = region === "NEM" ? true : false;
-const nemMaxY = useGW ? 33 : 33000;
+const region = 'NEM'; // or "WEM"
+const fts = region === 'NEM' ? nemFts : wemFts;
+const regionLength = region === 'NEM' ? 2017 : 337;
+const is5Min = region === 'NEM' ? true : false;
+const useGW = region === 'NEM' ? true : false;
+const nemMaxY = useGW ? 40 : 40000;
 const wemMaxY = useGW ? 3.3 : 3300;
-const maxY = region === "NEM" ? nemMaxY : wemMaxY;
-const showLast3Days = true
-const chartOptions = getChartOptions("opennem-widget-chart");
+const maxY = region === 'NEM' ? nemMaxY : wemMaxY;
+const showLast3Days = true;
+const chartOptions = getChartOptions('opennem-widget-chart');
 const dataURL = get7dPowerPath(region);
 
 fetch(dataURL)
@@ -24,11 +24,24 @@ fetch(dataURL)
   })
   .then(function (jsonData) {
     const data = jsonData.data ? jsonData.data : jsonData;
-    const dataset = dataTransform(data, regionLength, is5Min, fts, useGW, showLast3Days);
+    const dataset = dataTransform(
+      data,
+      regionLength,
+      is5Min,
+      fts,
+      useGW,
+      showLast3Days
+    );
     if (dataset.length > 0) {
-      drawChart(chartOptions, rollUp(dataset, fts.fuelTechIds), fts, maxY, useGW);
+      drawChart(
+        chartOptions,
+        rollUp(dataset, fts.fuelTechIds),
+        fts,
+        maxY,
+        useGW
+      );
     } else {
       // display no NEM. Try again later
-      console.log("There is no NEM or WEM data.");
+      console.log('There is no NEM or WEM data.');
     }
   });
