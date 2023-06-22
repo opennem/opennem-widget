350228587daedab7e422594c142f8083c33c1d05
Steven <steven@steventan.com.au>
2023-06-22 16:26:34 +1000

    add timestamp to filename bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserJSPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const timestamp = new Date().getTime();
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [
     new HtmlWebpackPlugin({
       minify: false,
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
     new FixStyleOnlyEntriesPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: "src/img/",
-          to: "img/[name].[ext]",
+          from: 'src/img/',
+          to: 'img/[name].[ext]',
         },
       ],
     }),
@@ -33,17 +35,17 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
   entry: {
-    "opennem-widget-polyfills": "./src/polyfills.js",
-    "opennem-widget": "./src/index.js",
-    "opennem-widget-styles": "./src/main.css",
+    'opennem-widget-polyfills': './src/polyfills.js',
+    'opennem-widget': './src/index.js',
+    'opennem-widget-styles': './src/main.css',
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: `[name]-${timestamp}.bundle.js`,
+    path: path.resolve(__dirname, 'dist'),
   },
 };
